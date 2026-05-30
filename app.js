98fef8693069b7f428ee9786f7ad62e0811a899d
Muttaqin86 <69788027+Muttaqin86@users.noreply.github.com>
2026-05-30 23:33:52 +0700

    test auto deploy

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('gitHub - CI/CD Jenkins Local Success 🚀')
+  res.send('GitHub - CI/CD Jenkins Local Success 🚀')
+  res.send('AUTO DEPLOY SUCCESS 🚀')
 })
 
 app.listen(4040, '0.0.0.0', () => {
